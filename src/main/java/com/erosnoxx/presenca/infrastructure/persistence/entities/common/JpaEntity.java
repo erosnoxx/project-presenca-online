01de4ea4a5868aa03b97a1ff1c320c39b382c951
Eros Vieira <garotoslixo@gmail.com>
2025-09-18 17:09:04 -0300

    chore: add getters and setters in abstract jpa entity

--- src/main/java/com/erosnoxx/presenca/infrastructure/persistence/entities/common/JpaEntity.java
+++ src/main/java/com/erosnoxx/presenca/infrastructure/persistence/entities/common/JpaEntity.java
@@ -1,10 +1,12 @@
 package com.erosnoxx.presenca.infrastructure.persistence.entities.common;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@MappedSuperclass
+@MappedSuperclass @Getter @Setter
 public abstract class JpaEntity<ID> {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
