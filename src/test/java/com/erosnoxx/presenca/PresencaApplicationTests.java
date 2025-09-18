62587ed9f0502fbce5241b991937b8d624070fe6
Eros Vieira <garotoslixo@gmail.com>
2025-09-18 13:04:07 -0300

    first commit >:P

---
+++ src/test/java/com/erosnoxx/presenca/PresencaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.erosnoxx.presenca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PresencaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
