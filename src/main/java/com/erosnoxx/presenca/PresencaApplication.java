84fd3ca6effba558b367fe90957eac897857628d
Eros Vieira <garotoslixo@gmail.com>
2025-09-18 14:06:37 -0300

    first commit >:P

---
+++ src/main/java/com/erosnoxx/presenca/PresencaApplication.java
@@ -0,0 +1,13 @@
+package com.erosnoxx.presenca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PresencaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PresencaApplication.class, args);
+	}
+
+}
