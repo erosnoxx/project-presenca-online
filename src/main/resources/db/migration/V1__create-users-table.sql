ce530924091cbf85f08c25d8b5b983b12499536c
Eros Vieira <garotoslixo@gmail.com>
2025-09-18 17:39:42 -0300

    feat: - add flyway dependency - create first migration (create users table) - add db creds on properties

---
+++ src/main/resources/db/migration/V1__create-users-table.sql
@@ -0,0 +1,13 @@
+-- V1__create_users_table.sql
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+CREATE TABLE users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    username VARCHAR(20) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    role VARCHAR(20) NOT NULL,
+
+    CONSTRAINT chk_role CHECK (role IN ('ADMIN', 'USER')),
+)
+
+CREATE INDEX idx_users_email ON users(username);
