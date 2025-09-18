95726bc57b40e79e426bf03f8af58efa87131c80
Eros Vieira <garotoslixo@gmail.com>
2025-09-18 14:34:29 -0300

    feat: add exceptions to invalid username and password

---
+++ src/main/java/com/erosnoxx/presenca/core/domain/exceptions/value_objects/InvalidPasswordException.java
@@ -0,0 +1,22 @@
+package com.erosnoxx.presenca.core.domain.exceptions.value_objects;
+
+import com.erosnoxx.presenca.core.domain.exceptions.common.DomainException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+
+public class InvalidPasswordException extends DomainException {
+    private final String detail;
+
+    public InvalidPasswordException(String detail) {
+        this.detail = detail;
+    }
+
+    @Override
+    public ProblemDetail toProblemDetail() {
+        var pb = ProblemDetail.forStatus(HttpStatus.UNPROCESSABLE_ENTITY);
+        pb.setTitle("Invalid password");
+        pb.setDetail(detail);
+
+        return pb;
+    }
+}
