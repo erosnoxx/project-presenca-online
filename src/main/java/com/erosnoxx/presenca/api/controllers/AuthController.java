756b2c613332fc2a0a78252b536b26e8aaef1c7b
Eros Vieira <garotoslixo@gmail.com>
2025-09-18 17:34:21 -0300

    feat: create auth controller and login endpoint

---
+++ src/main/java/com/erosnoxx/presenca/api/controllers/AuthController.java
@@ -0,0 +1,27 @@
+package com.erosnoxx.presenca.api.controllers;
+
+import com.erosnoxx.presenca.api.schemas.request.LoginRequest;
+import com.erosnoxx.presenca.api.schemas.response.LoginResponse;
+import com.erosnoxx.presenca.core.application.contracts.usecases.LoginUseCase;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController @RequestMapping("auth")
+public class AuthController {
+    private final LoginUseCase loginUseCase;
+
+    public AuthController(LoginUseCase loginUseCase) {
+        this.loginUseCase = loginUseCase;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(
+            @RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.ok(LoginResponse
+                        .fromOutput(loginUseCase.execute(request.toInput())));
+    }
+}
