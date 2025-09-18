2ebb39e330cad19954061fef1b0d97117739a0ca
Eros Vieira <garotoslixo@gmail.com>
2025-09-18 17:23:00 -0300

    chore: create toOutput method on TokenPair

--- src/main/java/com/erosnoxx/presenca/core/application/dto/TokenPair.java
+++ src/main/java/com/erosnoxx/presenca/core/application/dto/TokenPair.java
@@ -1,9 +1,14 @@
 package com.erosnoxx.presenca.core.application.dto;
 
+import com.erosnoxx.presenca.core.application.commands.output.LoginOutputCommand;
+
 import java.time.ZonedDateTime;
 
 public record TokenPair(
         String accessToken,
         String refreshToken,
         ZonedDateTime expiresAt) {
+    public LoginOutputCommand toOutput() {
+        return new LoginOutputCommand(accessToken, refreshToken, expiresAt);
+    }
 }
