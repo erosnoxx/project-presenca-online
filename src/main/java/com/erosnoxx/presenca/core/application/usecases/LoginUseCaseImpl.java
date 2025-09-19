48749a4127fbe941614c830913eeb49824039cec
Eros Vieira <garotoslixo@gmail.com>
2025-09-19 11:24:11 -0300

    chore: add username and password on login use case.

--- src/main/java/com/erosnoxx/presenca/core/application/usecases/LoginUseCaseImpl.java
+++ src/main/java/com/erosnoxx/presenca/core/application/usecases/LoginUseCaseImpl.java
@@ -2,20 +2,33 @@ package com.erosnoxx.presenca.core.application.usecases;
 
 import com.erosnoxx.presenca.core.application.commands.input.LoginInputCommand;
 import com.erosnoxx.presenca.core.application.commands.output.LoginOutputCommand;
+import com.erosnoxx.presenca.core.application.contracts.repositories.UserRepository;
 import com.erosnoxx.presenca.core.application.contracts.services.AuthenticatorService;
 import com.erosnoxx.presenca.core.application.contracts.usecases.LoginUseCase;
+import com.erosnoxx.presenca.core.domain.checkers.UserChecker;
+import com.erosnoxx.presenca.core.domain.exceptions.entities.UserNotAuthenticatedException;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LoginUseCaseImpl implements LoginUseCase {
     private final AuthenticatorService authenticatorService;
+    private final UserRepository repository;
 
-    public LoginUseCaseImpl(AuthenticatorService authenticatorService) {
+    public LoginUseCaseImpl(
+            AuthenticatorService authenticatorService,
+            UserRepository repository) {
         this.authenticatorService = authenticatorService;
+        this.repository = repository;
     }
 
     @Override
     public LoginOutputCommand execute(LoginInputCommand input) {
+        var user = repository.findByUsername(input.username())
+                .orElseThrow(() -> new UserNotAuthenticatedException("invalid username or password"));
+
+        if (!user.getPassword().matches(input.password()))
+            throw new UserNotAuthenticatedException("invalid username or password");
+
         return authenticatorService
                 .validateCredentials(input.username(), input.password())
                 .toOutput();
