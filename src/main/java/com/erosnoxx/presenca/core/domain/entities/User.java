77722b6d68682130888d3406d34025896f01dbb1
Eros Vieira <garotoslixo@gmail.com>
2025-09-19 10:59:45 -0300

    feat: add checker and create factory on User domain entity

--- src/main/java/com/erosnoxx/presenca/core/domain/entities/User.java
+++ src/main/java/com/erosnoxx/presenca/core/domain/entities/User.java
@@ -1,7 +1,9 @@
 package com.erosnoxx.presenca.core.domain.entities;
 
+import com.erosnoxx.presenca.core.domain.checkers.UserChecker;
 import com.erosnoxx.presenca.core.domain.entities.common.DomainEntity;
 import com.erosnoxx.presenca.core.domain.enums.Role;
+import com.erosnoxx.presenca.core.domain.exceptions.entities.UserAlreadyExistsException;
 import com.erosnoxx.presenca.core.domain.value_objects.Password;
 import com.erosnoxx.presenca.core.domain.value_objects.Username;
 import lombok.AllArgsConstructor;
@@ -17,6 +19,23 @@ public class User extends DomainEntity<UUID> {
     private Username username;
     private Password password;
     private Role role;
+
+    public static User create(
+            String username,
+            String password,
+            Role role,
+            UserChecker checker) {
+        if (!checker.isUsernameUnique(username))
+            throw new UserAlreadyExistsException("username already registered");
+
+        if (role == Role.ADMIN && checker.adminExists())
+            throw new UserAlreadyExistsException("admin user already registered");
+
+        return new User(
+                Username.of(username),
+                Password.fromPlainText(password),
+                role);
+    }
 
     public User(UUID id) {
         setId(id);
