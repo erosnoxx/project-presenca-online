c568793c7eec9faf9249b86a7041a6d36923aefc
Eros Vieira <garotoslixo@gmail.com>
2025-09-19 08:45:51 -0300

    chore: - add 'name.lastname' validation on username. - username now throws the correct exception (InvalidUsername)

--- src/main/java/com/erosnoxx/presenca/core/domain/value_objects/Username.java
+++ src/main/java/com/erosnoxx/presenca/core/domain/value_objects/Username.java
@@ -1,10 +1,15 @@
 package com.erosnoxx.presenca.core.domain.value_objects;
 
+import com.erosnoxx.presenca.core.domain.exceptions.value_objects.InvalidUsernameException;
 import com.erosnoxx.presenca.core.domain.value_objects.common.StringValueObject;
 
 public class Username extends StringValueObject {
     private static final int MIN_LENGTH = 5;
     private static final int MAX_LENGTH = 20;
+
+    public static Username of(String username) {
+        return new Username(username);
+    }
 
     public Username(String value) {
         super(value, MIN_LENGTH, MAX_LENGTH);
@@ -12,11 +17,16 @@ public class Username extends StringValueObject {
 
     @Override
     protected String customValidate(String value) {
-        return value;
+        var lower = value.toLowerCase();
+        if (!lower.matches("^[a-z]+\\.[a-z]+$")) {
+            throw createException("username must be in format 'name.lastname' with only letters");
+        }
+
+        return lower;
     }
 
     @Override
     protected RuntimeException createException(String message) {
-        return null;
+        return new InvalidUsernameException(message);
     }
 }
