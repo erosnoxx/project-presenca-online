f7d9a96cad058e823fe228168f9c02cb674b4605
Eros Vieira <garotoslixo@gmail.com>
2025-09-19 08:55:14 -0300

    test: - add tests to validate Password value object - add tests to validate Username value object - add 'test' profile annotation in the test application (todo: create application-test.properties)

--- src/test/java/com/erosnoxx/presenca/PresencaApplicationTests.java
+++ src/test/java/com/erosnoxx/presenca/PresencaApplicationTests.java
@@ -2,8 +2,9 @@ package com.erosnoxx.presenca;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest @ActiveProfiles("test")
 class PresencaApplicationTests {
 
 	@Test
