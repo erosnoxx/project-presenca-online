31bad83bbb5def15056b8c6b72c9a0e6024d3b89
Eros Vieira <garotoslixo@gmail.com>
2025-09-19 13:39:22 -0300

    feat: - add new methods on UserJpaRepository

--- src/main/java/com/erosnoxx/presenca/infrastructure/persistence/repositories/UserJpaRepository.java
+++ src/main/java/com/erosnoxx/presenca/infrastructure/persistence/repositories/UserJpaRepository.java
@@ -1,5 +1,6 @@
 package com.erosnoxx.presenca.infrastructure.persistence.repositories;
 
+import com.erosnoxx.presenca.core.domain.enums.Role;
 import com.erosnoxx.presenca.infrastructure.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,4 +12,7 @@ public interface UserJpaRepository extends JpaRepository<UserEntity, UUID> {
     UserDetails findUserByUsername(String username);
 
     Optional<UserEntity> findByUsername(String username);
+    boolean existsByUsername(String username);
+    boolean existsByRole(Role role);
+
 }
