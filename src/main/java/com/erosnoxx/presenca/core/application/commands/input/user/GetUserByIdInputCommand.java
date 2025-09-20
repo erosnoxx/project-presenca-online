f0d342a098b645dbd2d84abe23ea3db0c63c89aa
Eros Vieira <garotoslixo@gmail.com>
2025-09-20 10:02:33 -0300

    feat: add method 'of' in InputCommands

--- src/main/java/com/erosnoxx/presenca/core/application/commands/input/user/GetUserByIdInputCommand.java
+++ src/main/java/com/erosnoxx/presenca/core/application/commands/input/user/GetUserByIdInputCommand.java
@@ -3,4 +3,7 @@ package com.erosnoxx.presenca.core.application.commands.input.user;
 import java.util.UUID;
 
 public record GetUserByIdInputCommand(UUID id) {
+    public static GetUserByIdInputCommand of(UUID id) {
+        return new GetUserByIdInputCommand(id);
+    }
 }
--- src/main/java/com/erosnoxx/presenca/core/application/commands/input/user/GetUsersInputCommand.java
+++ src/main/java/com/erosnoxx/presenca/core/application/commands/input/user/GetUsersInputCommand.java
@@ -4,4 +4,7 @@ package com.erosnoxx.presenca.core.application.commands.input.user;
 import org.springframework.data.domain.Pageable;
 
 public record GetUsersInputCommand(Pageable pageable) {
+    public static GetUsersInputCommand of(Pageable pageable) {
+        return new GetUsersInputCommand(pageable);
+    }
 }
