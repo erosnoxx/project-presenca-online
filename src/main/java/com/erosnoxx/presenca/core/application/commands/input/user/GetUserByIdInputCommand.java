b6d8bd42ea2d8c1e8b61dc1c520d4f9bc60a27bc
Eros Vieira <garotoslixo@gmail.com>
2025-09-20 09:40:55 -0300

    feat: add GetUserById usecase

---
+++ src/main/java/com/erosnoxx/presenca/core/application/commands/input/user/GetUserByIdInputCommand.java
@@ -0,0 +1,6 @@
+package com.erosnoxx.presenca.core.application.commands.input.user;
+
+import java.util.UUID;
+
+public record GetUserByIdInputCommand(UUID id) {
+}
---
+++ src/main/java/com/erosnoxx/presenca/core/application/commands/output/user/GetUserByIdOutputCommand.java
@@ -0,0 +1,10 @@
+package com.erosnoxx.presenca.core.application.commands.output.user;
+
+import com.erosnoxx.presenca.core.application.dto.user.UserDto;
+import com.erosnoxx.presenca.core.domain.entities.User;
+
+public record GetUserByIdOutputCommand(UserDto user) {
+    public static GetUserByIdOutputCommand of(User user) {
+        return new GetUserByIdOutputCommand(UserDto.of(user));
+    }
+}
