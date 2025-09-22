9abd66f7c089b448f9109942c00cb01f05a5affb
Eros Vieira <garotoslixo@gmail.com>
2025-09-22 14:09:47 -0300

    chore: remove useless comments

--- src/test/java/com/erosnoxx/presenca/PresencaApplicationTests.java
+++ src/test/java/com/erosnoxx/presenca/PresencaApplicationTests.java
@@ -1,8 +1,12 @@
 package com.erosnoxx.presenca;
 
+import com.erosnoxx.presenca.core.application.contracts.usecases.classroom.CreateClassroomUseCase;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest @ActiveProfiles("test")
 class PresencaApplicationTests {
@@ -10,5 +14,5 @@ class PresencaApplicationTests {
 //	@Test
 //	void contextLoads() {
 //	}
-
 }
+
