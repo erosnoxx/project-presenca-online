558245521da7d20e4ecd98e8d5ff9c09166d1b2d
Eros Vieira <garotoslixo@gmail.com>
2025-09-22 11:04:02 -0300

    chore: refact UserMapper to use MapperUtils

--- src/main/java/com/erosnoxx/presenca/infrastructure/persistence/mappers/UserMapper.java
+++ src/main/java/com/erosnoxx/presenca/infrastructure/persistence/mappers/UserMapper.java
@@ -4,36 +4,40 @@ import com.erosnoxx.presenca.core.application.contracts.misc.EntityMapper;
 import com.erosnoxx.presenca.core.domain.entities.User;
 import com.erosnoxx.presenca.core.domain.value_objects.Password;
 import com.erosnoxx.presenca.core.domain.value_objects.Username;
+import com.erosnoxx.presenca.infrastructure.exceptions.EntityCannotBeMappedException;
 import com.erosnoxx.presenca.infrastructure.persistence.entities.UserEntity;
+import com.erosnoxx.presenca.infrastructure.persistence.mappers.common.MapperUtils;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper implements EntityMapper<User, UserEntity> {
     @Override
     public User toDomain(UserEntity persistence) {
-        var user = new User();
-        user.setId(persistence.getId());
-        user.setUsername(Username.of(persistence.getUsername()));
-        user.setPassword(Password.fromEncrypted(persistence.getPassword()));
-        user.setRole(persistence.getRole());
-        user.setCreatedAt(persistence.getCreatedAt());
-        user.setUpdatedAt(persistence.getUpdatedAt());
+        MapperUtils.validate(persistence.getClass(), MapperUtils.MapperType.PERSISTENCE);
 
-        return user;
+        var entity = new User();
+
+        MapperUtils.mapFromPersistence(entity, persistence);
+
+        entity.setUsername(Username.of(persistence.getUsername()));
+        entity.setPassword(Password.fromEncrypted(persistence.getPassword()));
+        entity.setRole(persistence.getRole());
+
+        return entity;
     }
 
     @Override
     public UserEntity toPersistence(User domain) {
-        var persistence = new UserEntity();
-        if (domain.getId() != null) {
-            persistence.setId(domain.getId());
-            persistence.setCreatedAt(domain.getCreatedAt());
-        }
+        MapperUtils.validate(domain.getClass(), MapperUtils.MapperType.DOMAIN);
 
-        persistence.setUsername(domain.getUsername().getValue());
-        persistence.setPassword(domain.getPassword().getValue());
-        persistence.setRole(domain.getRole());
+        var entity = new UserEntity();
 
-        return persistence;
+        MapperUtils.mapFromDomain(entity, domain);
+
+        entity.setUsername(domain.getUsername().getValue());
+        entity.setPassword(domain.getPassword().getValue());
+        entity.setRole(domain.getRole());
+
+        return entity;
     }
 }
